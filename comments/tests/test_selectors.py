156061c86f6a86ee806db7f4e2c22bb54bfb4009
bielpiot <bielpiot@gmail.com>
2022-08-16 18:45:18 +0200

    cleared repo

--- comments/tests/test_selectors.py
+++
@@ -1,7 +0,0 @@
-from articles.tests.articles_factories import ArticleFactory
-from django.test import TestCase
-
-
-class CommentsThreadTest(TestCase):
-    def setUp(self):
-        self.article1 = ArticleFactory()
--- common/types.py
+++
@@ -1,6 +0,0 @@
-from typing import TypeVar
-
-from django.db import models
-
-# source: Hacksoftware Django Styleguide
-DjangoModelType = TypeVar('DjangoModelType', bound=models.Model)
